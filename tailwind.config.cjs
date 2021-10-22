fe1e8a3117a44e1184ef900dd36b9c07bf1e9076
athirajohny <athira.mannapath@pearldatadirect.com>
2021-10-22 16:20:53 +0530

    added fonts and colour

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,33 @@
+const colors = require('tailwindcss/colors')
+
 const config = {
 	mode: 'jit',
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		fontFamily: {
+			'lato': ['Lato'],
+		   },
+		colors:{
+			purple:{
+				dark: '#604f80',
+				med :'#a76386',
+			},
+			custom:{
+				gray : '#373737',
+				lavender : '#fbeedf',
+				iceblue: '#b9beda',
+				yellow : '#ffc900',
+				violet: '#130f26',
+				green :'#02ba2a',
+			},
+			footer:{
+				light: '#f3f3f3',
+				dark :'#e3e3e3',
+			},
+			
+		},
 	},
 
 	plugins: []
